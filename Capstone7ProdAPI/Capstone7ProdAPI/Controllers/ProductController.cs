0898d9ffcd50185ce4e84c9a4394b32dd3031c07
samantha mazzola <smazzola7@gmail.com>
2018-09-05 19:50:26 -0400

    created new class

--- Capstone7ProdAPI/Capstone7ProdAPI/Controllers/ProductController.cs
+++ Capstone7ProdAPI/Capstone7ProdAPI/Controllers/ProductController.cs
@@ -7,18 +7,37 @@ using System.Web.Http;
 using Capstone7ProdAPI.Controllers;
 using Capstone7ProdAPI.Models;
 
+
 namespace Capstone7ProdAPI.Controllers
 {
+    public class Products
+    {
+        public int ProductID {set; get;}
+        public string ProductName {set; get;}
+    }
     public class ProductController : ApiController
     {
         public List<Product> GetAllProducts()
         {
-            //URL
+            //URL...... /api/Product/GetAllProducts
             //ORM
             northwindEntities ORM = new northwindEntities();
 
             List<Product> listOfProducts = ORM.Products.ToList();
 
+            return listOfProducts;
+        }
+
+        [HttpGet]
+        public List<Product> GetProductInfo(int ProductID)
+        {
+            //URL....
+            //ORM
+            northwindEntities ORM = new northwindEntities();
+
+            List<Product> listOfProducts = ORM.Products.Select(x => new Product
+            { ProductID = x.ProductID, ProductName = x.ProductName }).ToList();
+ 
             return listOfProducts;
         }
     }
